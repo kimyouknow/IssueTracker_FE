3d82c97f55936da9b885b19535384c33cd312c71
yunho <kimyouknow@naver.com>
2023-05-11 21:13:01 +0900

    Feat: DateInput 컴포넌트

---
+++ src/components/common/DateInput/DateInput.style.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  width: 383px;
+  height: 44px;
+  gap: 24px;
+  border-radius: 16px;
+`;
+
+export const Label = styled.label``;
+
+export const Input = styled.input``;
+
+export const Error = styled.span``;
