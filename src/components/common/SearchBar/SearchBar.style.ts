2ff18e5b55018f8785cd590b0ff2f5c5fbac893d
yunho <kimyouknow@naver.com>
2023-05-11 16:35:11 +0900

    Feat: SearchBar 컴포넌트

---
+++ src/components/common/SearchBar/SearchBar.style.ts
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: flex-start;
+  padding: 7px 8px;
+  gap: 3px;
+
+  width: 343px;
+  height: 36px;
+
+  color: ${({ theme: { colors } }) => colors.grayScale.gray_100};
+  background-color: ${({ theme: { colors } }) => colors.grayScale.gray_500};
+  border-radius: 10px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 22px;
+`;
+
+export const PlaceHolder = styled.span``;
