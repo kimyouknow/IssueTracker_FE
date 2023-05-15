caf09e9fa1e12aff019a81e76c9d6cc3090b5831
yunho <kimyouknow@naver.com>
2023-05-15 16:37:48 +0900

    Refactor: Progressive 컴포넌트 desktop버전으로 수정

--- src/components/common/Progressive/Progressive.style.ts
+++ src/components/common/Progressive/Progressive.style.ts
@@ -1,8 +1,38 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 244px;
+  height: 8px;
+  color: ${({ theme: { colors } }) => colors.grayScale.label};
+  background-color: ${({ theme: { colors } }) => colors.grayScale.background};
+`;
 
-export const Text = styled.h3`
-  color: ${({ theme: { colors } }) => colors.success.normal};
-  ${({ theme: { typography } }) => typography.link.large}
+export const Background = styled.div`
+  overflow: hidden;
+  height: inherit;
+  width: inherit;
+  border-radius: 10px;
+`;
+
+export const FilledBackground = styled.div<{ rate: number }>`
+  height: inherit;
+  width: ${({ rate }) => rate}%;
+  background-color: ${({ theme: { colors } }) => colors.primary.normal};
+`;
+
+export const DetailContainer = styled.div`
+  width: 100%;
+  height: 20px;
+  margin-top: 8px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Text = styled.span`
+  ${({ theme: { typography } }) => typography.text.xSmall};
+`;
+
+export const IssueContainer = styled.div`
+  display: flex;
+  gap: 8px;
 `;
--- src/components/common/Progressive/index.tsx
+++ src/components/common/Progressive/index.tsx
@@ -3,15 +3,35 @@ import { ProgressiveType } from './Progressive.type';
 
 export interface ProgressiveProps {
   type?: ProgressiveType;
-  rate: number;
+  openIssueCount: number;
+  closeIssueCount: number;
 }
 
-// TODO 모바일 버전에는 bar형태가 없음.
+const getProgressiveRate = (openIssueCount: number, closeIssueCount: number) => {
+  const totalIssues = openIssueCount + closeIssueCount;
+  if (totalIssues === 0) return 0;
+  const closedRate = closeIssueCount / totalIssues;
+  return Math.round(closedRate * 100);
+};
 
-const Progressive = ({ type = 'message', rate }: ProgressiveProps) => {
+const Progressive = ({ type = 'bar', openIssueCount, closeIssueCount }: ProgressiveProps) => {
+  const rate = getProgressiveRate(openIssueCount, closeIssueCount);
+  const isTextType = type === 'withMessage';
+
   return (
     <S.Container>
-      <S.Text>{rate}%</S.Text>
+      <S.Background>
+        <S.FilledBackground rate={rate} />
+      </S.Background>
+      {isTextType && (
+        <S.DetailContainer>
+          <S.Text>{rate}%</S.Text>
+          <S.IssueContainer>
+            <S.Text>열린 이슈 {openIssueCount}</S.Text>
+            <S.Text>닫힌 이슈 {closeIssueCount}</S.Text>
+          </S.IssueContainer>
+        </S.DetailContainer>
+      )}
     </S.Container>
   );
 };
