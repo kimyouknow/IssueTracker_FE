32120b14f15df089457c2c56fadf66a9e6dd6b56
yunho <kimyouknow@naver.com>
2023-05-23 20:55:18 +0900

    Refactor: userMockHandler의 핸들러 key값 설정

--- src/mocks/user/user.mockHandler.ts
+++ src/mocks/user/user.mockHandler.ts
@@ -7,7 +7,7 @@ import { createRandomMembers } from './user.mock';
 const userMockHandler = [
   // GET: api/members
   rest.get(ROOT_API_URL + REQUEST_API.USER.MEMBER, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(createRandomMembers(10)));
+    return res(ctx.status(200), ctx.json({ members: createRandomMembers(10) }));
   }),
 ];
 
